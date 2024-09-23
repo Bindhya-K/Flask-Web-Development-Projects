e5a4013c0c3dac27a80897c5cd014e997233196f
Bindhya-K <87693783+Bindhya-K@users.noreply.github.com>
2024-09-23 05:54:55 +0000

    added html page

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return "Hello World"
+  return render_template("home.html")
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port=80, debug=True)
